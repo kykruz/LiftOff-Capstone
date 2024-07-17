5ee4afe6e0c24ba4c901e8e87b29ec09d877216f
Ricky <ricky@Luiss-MacBook-Air.local>
2024-07-17 13:18:30 -0500

    something is erroring in HomeController

--- Itinerary-Designer/Controllers/HomeController.cs
+++ Itinerary-Designer/Controllers/HomeController.cs
@@ -60,14 +60,14 @@ public class HomeController : Controller
     {
         try
         {
-            Exchange.Services.ConvertRequest request = new Exchange.Services.ConvertRequest(fromCurrency, toCurrency, amount);
-            Exchange.Services.ConvertResponse response = await _exchangeRatesApi.ConvertAsync(request);
+            // Exchange.Services.ConvertRequest request = new Exchange.Services.ConvertRequest(fromCurrency, toCurrency, amount);
+            // Exchange.Services.ConvertResponse response = await _exchangeRatesApi.ConvertAsync(request);
 
             //Preparing data to pass to the View
-            ViewData["Amount"] = amount;
-            ViewData["FromCurrency"] = fromCurrency;
-            ViewData["ToCurrency"] = toCurrency;
-            ViewData["ConvertedAmount"] = response.Result;
+            // ViewData["Amount"] = amount;
+            // ViewData["FromCurrency"] = fromCurrency;
+            // ViewData["ToCurrency"] = toCurrency;
+            // ViewData["ConvertedAmount"] = response.Result;
 
             return View("Index");
         }
