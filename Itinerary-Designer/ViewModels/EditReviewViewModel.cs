4d78d6c10cb39ae6a0be3362b8e6911808777996
Ricky <ricky@luiss-air.mynetworksettings.com>
2024-07-24 13:26:29 -0500

    Figuring out Edit & Delete 4 Review Page

---
+++ Itinerary-Designer/ViewModels/EditReviewViewModel.cs
@@ -0,0 +1,20 @@
+// using System;
+// using System;
+// using System.Collections.Generic;
+// using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
+// using Microsoft.VisualStudio.Web.CodeGenerators.Mvc;
+// using Trips.Models;
+
+// namespace EditReviewViewModel
+// {
+//     public class EditReviewViewModel
+//     {
+//         public string? Username { get; set; }
+//         public string? Title { get; set; }
+//         public string? ReviewPost { get; set; }
+//         public int ReviewId { get; set; }
+
+//         public EditReviewViewModel() { }
+//     }
+// }
